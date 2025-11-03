3faabc129f214e95c4c270f5043dae93e1e39703
dspajio <17087568505@163.com>
2025-11-03 21:11:51 +0800

    测试v0.1

--- Core/Src/main.c
+++ Core/Src/main.c
@@ -94,7 +94,7 @@ int main(void)
   MX_USART1_UART_Init();
   /* USER CODE BEGIN 2 */
   M2006_InitAll(motors, &hcan1);
-
+  User_Uart_Init(&huart1);
   /* USER CODE END 2 */
 
   /* Infinite loop */
