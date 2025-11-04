0754df1edda9c3bf40a178361c9157e795913a01
dspajio <17087568505@163.com>
2025-11-05 02:17:42 +0800

    测试v0.1

--- Core/Src/main.c
+++ Core/Src/main.c
@@ -56,8 +56,8 @@ void SystemClock_Config(void);
 
 /* Private user code ---------------------------------------------------------*/
 /* USER CODE BEGIN 0 */
-M2006_t motors[2];
-int8_t dir[2] = { +1, -1 };
+//M2006_t motors[2];
+//int8_t dir[2] = { +1, -1 };
 /* USER CODE END 0 */
 
 /**
@@ -93,7 +93,7 @@ int main(void)
   MX_USART6_UART_Init();
   MX_USART1_UART_Init();
   /* USER CODE BEGIN 2 */
-  M2006_InitAll(motors, &hcan1);
+  //M2006_InitAll(motors, &hcan1);
   User_Uart_Init(&huart6);
   /* USER CODE END 2 */
 
@@ -101,7 +101,9 @@ int main(void)
   /* USER CODE BEGIN WHILE */
   while (1)
   {
-    demo_ping();
+    //demo_ping();
+    HAL_Delay(1000);
+    HAL_UART_Transmit(&huart1, "test_data", 10, 100);
     /* USER CODE END WHILE */
 
     /* USER CODE BEGIN 3 */
@@ -126,12 +128,11 @@ void SystemClock_Config(void)
   /** Initializes the RCC Oscillators according to the specified parameters
   * in the RCC_OscInitTypeDef structure.
   */
-  RCC_OscInitStruct.OscillatorType = RCC_OSCILLATORTYPE_HSI;
-  RCC_OscInitStruct.HSIState = RCC_HSI_ON;
-  RCC_OscInitStruct.HSICalibrationValue = RCC_HSICALIBRATION_DEFAULT;
+  RCC_OscInitStruct.OscillatorType = RCC_OSCILLATORTYPE_HSE;
+  RCC_OscInitStruct.HSEState = RCC_HSE_ON;
   RCC_OscInitStruct.PLL.PLLState = RCC_PLL_ON;
-  RCC_OscInitStruct.PLL.PLLSource = RCC_PLLSOURCE_HSI;
-  RCC_OscInitStruct.PLL.PLLM = 8;
+  RCC_OscInitStruct.PLL.PLLSource = RCC_PLLSOURCE_HSE;
+  RCC_OscInitStruct.PLL.PLLM = 6;
   RCC_OscInitStruct.PLL.PLLN = 168;
   RCC_OscInitStruct.PLL.PLLP = RCC_PLLP_DIV2;
   RCC_OscInitStruct.PLL.PLLQ = 4;
