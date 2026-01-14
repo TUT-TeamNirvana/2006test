3a46f7725e11a25793652eba4616be8253c473e0
dspajio <17087568505@163.com>
2026-01-14 22:20:52 +0800

    Extra,4摩擦轮特供v0.1,首次测试

--- Core/Src/main.c
+++ Core/Src/main.c
@@ -177,9 +177,9 @@ int main(void)
   M2006_SetControlMode(&motors[1], M2006_MODE_SPEED);
   M2006_SetControlMode(&motors[2], M2006_MODE_SPEED);
   //M2006_SetControlMode(&motors[3], M2006_MODE_SPEED);
-  M2006_SetSpeedTarget(&motors[0], dir[0] * 100.0f);
-  M2006_SetSpeedTarget(&motors[1], dir[0] * 200.0f);
-  M2006_SetSpeedTarget(&motors[2], dir[1] * 200.0f);
+  M2006_SetSpeedTarget(&motors[0], dir[1] * 100.0f);
+  M2006_SetSpeedTarget(&motors[1], dir[2] * 200.0f);
+  M2006_SetSpeedTarget(&motors[2], dir[3] * 200.0f);
   //M2006_SetSpeedTarget(&motors[3], dir[1] * 5000.0f);
   /*M2006_SetPosTarget(&motors[0], dir[0] * 0.0f);
   M2006_SetPosTarget(&motors[1], dir[1] * 0.0f);*/
