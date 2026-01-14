337a226cc13799ca89762feba29ccd43fd8f187e
dspajio <17087568505@163.com>
2026-01-15 03:55:34 +0800

    Extra,英雄摩擦轮发射评测完成。usb初步尝试，cubemx配置完毕，已确认可行性，尝试移植代码

---
+++ Core/bsp/usb/usb.h
@@ -0,0 +1,13 @@
+//
+// Created by 17087 on 26-1-15.
+//
+
+#ifndef USB_H
+#define USB_H
+
+#include "usbd_cdc_if.h"
+#include <stdio.h>
+
+int _write(int file, char *ptr, int len);
+
+#endif //USB_H
